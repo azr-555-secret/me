4a079bdb1794c3ae7acaf551a0703fd4eee8b013
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2020-03-28 06:53:03 +0700

    Add files via upload

---
+++ ui.php
@@ -0,0 +1,7 @@
+<?php
+echo color("green","* * * * *"); echo color("nevy","[ AUTO CREATE ACCOUNT ]"); echo color("green","* * * * *")."\n";
+echo color("green","* * * * *"); echo color("nevy","[ AUTO CLAIM VOUCHERS ]"); echo color("green","* * * * *")."\n";
+echo color("green","* * * * * * *"); echo color("nevy","[ AUTO SET PIN ]"); echo color("green","* * * * * *")."\n";
+echo color("green","* * * * *"); echo color("nevy"," Created by : Vino AZR"); echo color("green","* * * * *")."\n";
+echo color("green","* * * * *"); echo color("nevy"," Version    : 555"); echo color("green","* * * * *")."\n";
+echo color("green","* * * * *"); echo color("nevy"," Time       : [22-02-2020] [06:44:16]"); echo color("green","* * * * *")."\n";
