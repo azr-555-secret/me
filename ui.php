110c3b32bce93b69db79491fd684e3c5419fa6a1
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2020-03-28 08:12:52 +0700

    Update ui.php

--- ui.php
+++ ui.php
@@ -2,18 +2,18 @@
 date_default_timezone_set('Asia/Jakarta');
 include "func.php";
 
-echo color("green"," * * * * * * * * * * * * * * * * * * * * * * ")."\n";
-echo color("green"," *           "); echo color("purple","AUTO CREATE ACCOUNT"); echo color("green","           * ")."\n";
-echo color("green"," *           "); echo color("purple","AUTO CLAIM VOUCHERS"); echo color("green","           * ")."\n";
-echo color("green"," *               "); echo color("purple","AUTO SET PIN"); echo color("green","              * ")."\n";
-echo color("green"," *          "); echo color("nevy","Created by : Vino AZR"); echo color("green","          * ")."\n";
-echo color("green"," *             "); echo color("nevy","Version : 555"); echo color("green","               * ")."\n";
-echo color("green"," *       "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","      * ")."\n";
-echo color("green"," * * * * * * * * * * * * * * * * * * * * * * ")."\n\n";
-echo color("purple"," * * * * * [ AUTO CREATE ACCOUNT ] * * * * *")."\n";
-echo color("purple"," * * * * * [ AUTO CLAIM VOUCHERS ] * * * * *")."\n";
-echo color("purple","     * * * * * [ AUTO SET PIN ] * * * * *")."\n";
+echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n";
+echo color("green","     *           "); echo color("purple","AUTO CREATE ACCOUNT"); echo color("green","           * ")."\n";
+echo color("green","     *           "); echo color("purple","AUTO CLAIM VOUCHERS"); echo color("green","           * ")."\n";
+echo color("green","     *               "); echo color("purple","AUTO SET PIN"); echo color("green","              * ")."\n";
+echo color("green","     *          "); echo color("nevy","Created by : Vino AZR"); echo color("green","          * ")."\n";
+echo color("green","     *             "); echo color("nevy","Version : 555"); echo color("green","               * ")."\n";
+echo color("green","     *       "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","      * ")."\n";
+echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n\n";
+echo color("purple","    * * * * * [ AUTO CREATE ACCOUNT ] * * * * * ")."\n";
+echo color("purple","    * * * * * [ AUTO CLAIM VOUCHERS ] * * * * * ")."\n";
+echo color("purple","         * * * * * [ AUTO SET PIN ] * * * * * ")."\n";
 
-echo color("green"," * * * * * * * * * * * * * * * * * * * * * * ")."\n";
-echo color("green"," *                   "); echo color("nevy","DONE"); echo color("green","                  * ")."\n";
-echo color("green"," * * * * * * * * * * * * * * * * * * * * * * ")."\n";
+echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n";
+echo color("green","     *                   "); echo color("nevy","DONE"); echo color("green","                  * ")."\n";
+echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n";
