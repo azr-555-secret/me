94661ac76724eb7fb362022aae73ace695f3a93b
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2020-03-28 08:15:30 +0700

    Update ui.php

--- ui.php
+++ ui.php
@@ -10,10 +10,10 @@ echo color("green","     *          "); echo color("nevy","Created by : Vino AZR
 echo color("green","     *             "); echo color("nevy","Version : 555"); echo color("green","               * ")."\n";
 echo color("green","     *       "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","      * ")."\n";
 echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n\n";
-echo color("purple","    * * * * * [ AUTO CREATE ACCOUNT ] * * * * * ")."\n";
-echo color("purple","    * * * * * [ AUTO CLAIM VOUCHERS ] * * * * * ")."\n";
-echo color("purple","         * * * * * [ AUTO SET PIN ] * * * * * ")."\n";
+echo color("purple","     * * * * *   AUTO CREATE ACCOUNT   * * * * * ")."\n";
+echo color("purple","     * * * * *   AUTO CLAIM VOUCHERS   * * * * * ")."\n";
+echo color("purple","         * * * * *   AUTO SET PIN   * * * * * ")."\n";
 
 echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n";
-echo color("green","     *                   "); echo color("nevy","DONE"); echo color("green","                  * ")."\n";
+echo color("green","     *                   "); echo color("purple","DONE"); echo color("green","                  * ")."\n";
 echo color("green","     * * * * * * * * * * * * * * * * * * * * * * ")."\n";
