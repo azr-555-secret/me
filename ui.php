b8b657b4d17486017615f60c02825ad2bb65b0d7
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2020-03-28 07:34:37 +0700

    Update ui.php

--- ui.php
+++ ui.php
@@ -2,11 +2,11 @@
 date_default_timezone_set('Asia/Jakarta');
 include "func.php";
 
-echo color("green"," ***************************************** ")."\n";
+echo color("green"," * * * * * * * * * * * * * * * * * * * * * ")."\n";
 echo color("green"," *        "); echo color("purple","[ AUTO CREATE ACCOUNT ]"); echo color("green","        * ")."\n";
 echo color("green"," *        "); echo color("purple","[ AUTO CLAIM VOUCHERS ]"); echo color("green","        * ")."\n";
 echo color("green"," *            "); echo color("purple","[ AUTO SET PIN ]"); echo color("green","           * ")."\n";
 echo color("green"," *         "); echo color("nevy","Created by : Vino AZR"); echo color("green","         * ")."\n";
 echo color("green"," *            "); echo color("nevy","Version : 555"); echo color("green","              * ")."\n";
 echo color("green"," *      "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","     * ")."\n";
-echo color("green"," ***************************************** ")."\n";
+echo color("green"," * * * * * * * * * * * * * * * * * * * * * ")."\n";
