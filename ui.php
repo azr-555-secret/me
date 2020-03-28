44d81037e2ff18460153410585091d01e2867dfd
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2020-03-28 07:51:06 +0700

    Update ui.php

--- ui.php
+++ ui.php
@@ -2,16 +2,14 @@
 date_default_timezone_set('Asia/Jakarta');
 include "func.php";
 
-for($a=1;$a<=21;$a++) {
-	echo color("yellow"," *");
-	sleep(0.1);
-}
-
 echo color("green"," * * * * * * * * * * * * * * * * * * * * * ")."\n";
 echo color("green"," *        "); echo color("purple","[ AUTO CREATE ACCOUNT ]"); echo color("green","        * ")."\n";
 echo color("green"," *        "); echo color("purple","[ AUTO CLAIM VOUCHERS ]"); echo color("green","        * ")."\n";
 echo color("green"," *            "); echo color("purple","[ AUTO SET PIN ]"); echo color("green","           * ")."\n";
 echo color("green"," *         "); echo color("nevy","Created by : Vino AZR"); echo color("green","         * ")."\n";
 echo color("green"," *            "); echo color("nevy","Version : 555"); echo color("green","              * ")."\n";
-echo color("green"," *      "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","     * ")."\n";
+echo color("green"," *      "); echo color("nevy","Date : ".date('d-m-Y | H:i:s')); echo color("green","     * ")."\n\n";
 echo color("green"," * * * * * * * * * * * * * * * * * * * * * ")."\n";
+echo color("purple"," * * * * *[ AUTO CREATE ACCOUNT ]* * * * *")."\n";
+echo color("purple"," * * * * *[ AUTO CLAIM VOUCHERS ]* * * * *")."\n";
+echo color("purple"," * * * * *[ AUTO SET PIN ]* * * * *")."\n";
